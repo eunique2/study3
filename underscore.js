a5701e4190d394760216462c803d8791aa2325e8
eunique2 <eunique@dongwha.com>
2018-02-24 00:21:27 +0900

    callback

--- underscore.js
+++ underscore.js
@@ -5,3 +5,8 @@ console.log(_.first(arr));
 
 console.log(arr[arr.length-1]);
 console.log(_.last(arr));
+
+
+console.log(arr.sort(function(a,b){console.log('c',a,b); return a-b;}));
+
+console.log(arr.sort());
