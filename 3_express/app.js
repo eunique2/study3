d677b7d91c37c5d598113b1f3b7ca153ae4efae3
eunique2 <eunique@dongwha.com>
2018-02-24 17:27:34 +0900

    static vs dynamic

--- 3_express/app.js
+++ 3_express/app.js
@@ -3,18 +3,42 @@ const app = express();
 
 app.use(express.static('3_express/public'));
 
-app.get('/',function(req,res){
+/**
+ route
+*/
+app.get('/', function(req, res) {
   res.send('hello!!');
 });
 
-app.get('/logo',function(req,res){
+app.get('/dynamic', function(req, res) {
+  var lis = '';
+  for(var i=0; i<5; i++){
+
+  }
+  var output = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+      Hello dynamic :)
+    </body>
+  </html>
+`;
+  res.send(output);
+});
+
+app.get('/logo', function(req, res) {
   res.send('<img src="/logo70th.gif"/>');
 });
 
-app.get('/login',function(req,res){
+app.get('/login', function(req, res) {
   res.send('Login please..');
 });
 
-app.listen(3000,function(){
+
+app.listen(3000, function() {
   console.log('express server is started!!!');
 });
